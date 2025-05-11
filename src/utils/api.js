051414ded656cbdf45ec46fee95017dd1662aa1c
Zehra <zehra.s2832@gmail.com>
2025-05-11 18:05:17 +0500

    backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/admin',
+  baseURL: 'https://rephone-backend.vercel.app/api/admin',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
